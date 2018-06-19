60b7f21e69d786add5a0dac4829438f9875b8604
Mohit Nihalani <nihalanim9@gmail.com>
2018-06-19 11:30:34 -0400

    added file to store tweets

--- search.js
+++ search.js
@@ -1,24 +1,37 @@
-var Twitter = require('twitter'); // this is how we import the twit package
-var config = require('./config') //this is we import the config 
 
-var T = new Twitter(config); //this is the object of twit which 
+require('dotenv').config();
+var Twitter = require('twitter'); // this is how we import the twit package
+var config = require('./config'); //this is we import the config 
+var fs = require('fs');
+
+console.log(config.access_token_key)
+var T = new Twitter(config);
 var params = { 
-    q: 'Donald Trump',
+    q: 'donald trump',
      count: 100 
     }
 //'search/tweets'
-
-	
-console.log('The value of PORT is:', process.env.PORT);
-/** 
+var util = require('util')
+let new_data = []
+console.log(typeof(new_data))
 T.get('search/tweets', params,searchedData);
 function searchedData(err, data, response) 
-{
-     
-        for(let i = 0; i < data.statuses.length; i++)
-        {
-            console.log(data.statuses[i].text)
-        }
-         
+{	j =0;
+
+		for (i=0;i<data.statuses.length;i++){
+			new_data.push(i + " " + data.statuses[i].text)
+
+     		//console.log(data)
+     	}
+
+     		new_data.toString();
+     		
+     fs.writeFileSync('index.txt', 'var obj = ' + util.inspect(new_data) , 'utf-8')
+       
+     console.log(new_data)  
 }
-*/
+
+
+
+	     		
+			
